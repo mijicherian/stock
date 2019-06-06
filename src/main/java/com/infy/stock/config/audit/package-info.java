dcfbcf97882179d8572e875f95101e3748c207fa
default <default>
2019-06-06 10:17:41 +0000

    Adding initial code structure

---
+++ src/main/java/com/infy/stock/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.infy.stock.config.audit;
---
+++ src/main/java/com/infy/stock/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.infy.stock.web.rest.vm;
